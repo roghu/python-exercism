46ab949a262311b289bc8aec42243230ab1bb754
Roger Hughes <roger@roghu.com>
2021-08-07 15:31:20 -0400

    test: Get nox working with pdm

--- noxfile.py
+++ noxfile.py
@@ -1,27 +1,33 @@
+import os
+
 import nox
+
+os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
 
 
 @nox.session
 def tests(session: nox.Session) -> None:
-    session.run("pip", "install", "-r", "requirements.txt")
+    session.run("pdm", "install", "-ds", "test", external=True)
     session.run("pytest", "-n", "2")
 
 
 @nox.session
 def flake8(session: nox.Session) -> None:
-    session.run("pip", "install", "-r", "requirements.txt")
+    session.run("pdm", "install", "-ds", "test", external=True)
+    session.run("pdm", "install", "-ds", "flake8", external=True)
     session.run("pytest", "-n", "2", "--flake8")
 
 
 @nox.session
 def typing(session: nox.Session) -> None:
-    session.run("pip", "install", "-r", "requirements.txt")
+    session.run("pdm", "install", "-ds", "test", external=True)
+    session.run("pdm", "install", "-ds", "typing", external=True)
     session.run("pytest", "-n", "2", "--mypy")
 
 
 @nox.session
 def coverage(session: nox.Session) -> None:
-    session.run("pip", "install", "-r", "requirements.txt")
+    session.run("pdm", "install", "-ds", "test", external=True)
     session.run("pytest", "-n", "2", "--cov", "src/")
     session.run("coverage", "xml")
     session.run("coverage", "report", "--show-missing")
