a1101d6a7524a2d22a2627dcb274d86511da2353
Roger Hughes <roger@roghu.com>
2021-08-09 12:08:04 -0400

    feat: Add pangram

---
+++ src/pangram.py
@@ -0,0 +1,8 @@
+import re
+
+
+def is_pangram(sentence: str) -> bool:
+    if not sentence:
+        return False
+    sentence = re.sub(r"[\W\d_]", '', sentence)
+    return len(set(sentence.lower())) == 26
