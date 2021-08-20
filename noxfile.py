af6ea5be2b1bc8be29e8483ef74d127aab9a9873
Roger Hughes <roger@roghu.com>
2021-08-20 10:07:46 -0400

    chore: Change flag due to pdm change
    PDM deprecate `-s` flag for section install. Using `-G` flag instead
    See pdm issue: https://github.com/pdm-project/pdm/issues/591

--- noxfile.py
+++ noxfile.py
@@ -10,27 +10,27 @@ nox.options.stop_on_first_error = True
 
 @nox.session
 def tests(session: nox.Session) -> None:
-    session.run("pdm", "install", "-ds", "test", external=True)
+    session.run("pdm", "install", "-G", "test", external=True)
     session.run("pytest", "-n", "2")
 
 
 @nox.session
 def flake8(session: nox.Session) -> None:
-    session.run("pdm", "install", "-ds", "test", external=True)
-    session.run("pdm", "install", "-ds", "flake8", external=True)
+    session.run("pdm", "install", "-G", "test", external=True)
+    session.run("pdm", "install", "-G", "flake8", external=True)
     session.run("pytest", "-n", "2", "--flake8")
 
 
 @nox.session
 def typing(session: nox.Session) -> None:
-    session.run("pdm", "install", "-ds", "test", external=True)
-    session.run("pdm", "install", "-ds", "typing", external=True)
+    session.run("pdm", "install", "-G", "test", external=True)
+    session.run("pdm", "install", "-G", "typing", external=True)
     session.run("pytest", "-n", "2", "--mypy")
 
 
 @nox.session
 def coverage(session: nox.Session) -> None:
-    session.run("pdm", "install", "-ds", "test", external=True)
+    session.run("pdm", "install", "-G", "test", external=True)
     session.run("pytest", "-n", "2", "--cov", "src/")
     session.run("coverage", "xml")
     session.run("coverage", "report", "--show-missing")
